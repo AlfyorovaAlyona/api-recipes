320812fd77ba4a6ee8a78afb63fade1f788934f8
AlfyorovaAlyona <alyona02121999@gmail.com>
2023-08-28 14:46:11 +0300

    added user create post request and test for it

---
+++ app/user/views.py
@@ -0,0 +1,7 @@
+""""Views fro User API"""
+from rest_framework import generics
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
