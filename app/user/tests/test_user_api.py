8c560d5de6009f4919f3817f1007c6bdd3457dc7
AlfyorovaAlyona <alyona02121999@gmail.com>
2023-08-28 16:48:48 +0300

    added authentication and tests

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,6 +6,8 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -53,3 +55,80 @@ class PublicUserApiTests(TestCase):
             email=['email']
         ).exists()
         self.assertFalse(user_exists)  # User should not be created
+
+    def test_create_token_for_user(self):
+        usr_details = {
+            'email': 'test@example.com',
+            'password': 'password12345',
+            'name': 'Test username',
+        }
+        create_user(**usr_details)
+
+        payload = {
+            'email': usr_details['email'],
+            'password': usr_details['password'],
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_user_bad_credential(self):
+        create_user(email='test@example.com', password='pass1234')
+
+        payload = {
+            'email': 'test@example.com',
+            'password': 'not-pass1234'
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        payload = {
+            'email': 'test@example.com',
+            'password': ''
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+
+    def setUp(self):
+        self.user = create_user(
+            email='test2@example.com',
+            password='passwordforuser',
+            name='Test private user',
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email,
+        })
+
+    def test_post_me_not_allowed(self):
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        payload = {'name': 'Updated name', 'password': 'newpassfor user'}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,30 @@
 """"Views fro User API"""
-from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework import generics, authentication, permissions
+from rest_framework.settings import api_settings
+from rest_framework.authtoken.views import ObtainAuthToken
+
+from user.serializers import (
+    AuthTokenSerializer,
+    UserSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class CreateTokeView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    # is the user is what it claims to be
+    authentication_classes = [authentication.TokenAuthentication]
+    # is the user allowed to do what it wants to do
+    permission_classes = [permissions.IsAuthenticated]
+
+    # Overriding get_object() method
+    def get_object(self):
+        return self.request.user
