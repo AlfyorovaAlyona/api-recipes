885fddf9390376b69e8f0ce6194ebee905245ec2
AlfyorovaAlyona <alyona02121999@gmail.com>
2023-08-30 13:48:49 +0300

    added tag instance and tests for it

--- app/core/models.py
+++ app/core/models.py
@@ -55,7 +55,19 @@ class Recipe(models.Model):
     time_min = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('tag')
 
     # Set str() method to return Title of recipe
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
